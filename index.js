277d0f48962f14242b3027f9a57d62166eb307b6
victor <victor.odhi@gmail.com>
2023-09-19 14:45:11 +0300

    compplete test

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 18;
+const num4 = 7;
+const multiply = num1 * num2
+const mod = num3 % num4 
+let random = Math.floor(Math.random() * 10) + 1;
+let max = Math.max(1, 2, 3, 4, 20);
